453247c3248ba5f848a953fa204a040aed0fe6c6
Ilmi Udisha <139669406+Ilmiudisha@users.noreply.github.com>
2025-03-21 04:17:02 +0000

    Implemented a Java program to display odd numbers from 1 to 20 using a for loop.

---
+++ Ex04.java
@@ -0,0 +1,9 @@
+//4)print the odd numbers between 1 to 20
+class Ex04{
+    public static void main(String args[]){
+
+        for(int i=1; i<=20; i+=2){
+            System.out.println(i);
+        }
+    }
+}
